eed09fdb895b26620e2210ea6a82e0344109cd88
Ciro Pallaro <ciropallaro@gmail.com>
2025-04-21 14:24:47 -0300

    Agregando lo que pide el desafio

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,11 @@
 alert("Alerta desde js")
 
 console.log("Hola desde main.js")
+
+const nombreProducto = "Bondiola"
+const precioUnitario = "3000"
+let cantidadDeseada = prompt("Cuantas bondiolas quieres comprar?")
+
+precioTotal = precioUnitario * cantidadDeseada
+
+alert("El coste total es de: " + precioTotal)
